04f35ad9157f854421cd3842830e6125557a1303
lebed <lebed@DESKTOP-1RAG4Q9>
2021-10-22 20:58:26 +0300

    Добавьте файлы проекта.

---
+++ ProgressBarDance_1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace ProgressBarDance_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество баров";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(173, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(54, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox1_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(43, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(713, 284);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(209, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(439, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 48);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сброс";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(831, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(239, 277);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ProgressBarDance_1/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProgressBarDance_1
+{
+    public partial class Form1 : Form
+    {
+        private delegate void Safemethod(int numberBar);
+        private delegate void Safeme(object numb);
+        List<VerticalProgressBar> verticals;
+        Size sizeBar;
+        int countThread;
+        ThreadStart [] threadStarts;
+        Thread[] threads;
+        Safemethod safemethod;
+        Safeme temp;
+        //TimerCallback callback;
+        //System.Threading.Timer timer;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            safemethod = new Safemethod(Foo);
+           temp = new Safeme(foo2);
+            verticals = new List<VerticalProgressBar>();
+            //callback = new TimerCallback(MyPerformStep);
+            //timer= new System.Threading.Timer(callback);
+            sizeBar = new Size(60, groupBox1.Height - 20);
+            comboBox1.Text = comboBox1.Items[0].ToString();           
+            this.StartPosition = FormStartPosition.CenterScreen;
+            button2.Enabled = false;
+        }
+        public void MyPerformStep(object obj)
+        {
+            System.Threading.Timer timer1=obj as System.Threading.Timer ;
+            Random r = new Random();
+               verticals[r.Next(0,countThread)].Invoke(safemethod, r.Next(0, countThread));
+            // listBox1.Invoke(temp, (object)Thread.CurrentThread.ManagedThreadId);
+            
+            //if (verticals[r.Next(0, countThread)].Value == verticals[r.Next(0, countThread)].Maximum)
+            //     timer1.Dispose();
+        }
+        public void foo2(object t)
+        {
+            listBox1.Items.Add($"Поток: {t.ToString()}");
+        }
+        public void Foo(int numberBar)
+        {            
+            if(countThread!=0)
+            {
+                int t = Thread.CurrentThread.ManagedThreadId;
+                verticals[numberBar].PerformStep();
+                listBox1.Items.Add($"Поток: {t.ToString()}");
+            }           
+        }
+        public void LetsDance()
+        {
+            TimerCallback callback = new TimerCallback(MyPerformStep);
+            System.Threading.Timer timer=new System.Threading.Timer(callback);
+            timer.Change(0, 100);
+            
+        }
+
+        public void LetsDance1(object a)
+        {
+            TimerCallback callback = new TimerCallback(MyPerformStep);
+            System.Threading.Timer timer = new System.Threading.Timer(callback);
+            timer.Change(0, 100);
+
+        }
+
+        //Кнопка старт.
+        private void button1_Click(object sender, EventArgs e)
+        {           
+            if (button2.Enabled == false)
+            {
+                button2.Enabled = true;
+            }
+            comboBox1.Enabled = false;
+            button1.Enabled = false;
+            //Число потоков по числу баров.
+            countThread = comboBox1.SelectedIndex+1;
+            //Массив потоков.
+            threads = new Thread[countThread];
+            //Массив делегатов.
+            threadStarts = new ThreadStart[countThread];
+            //Инициализация потоков и их запуск.
+            for (int i=0;i<countThread;i++)
+            {
+                //threadStarts[i] = new ThreadStart(LetsDance);
+                //threads[i] = new Thread(threadStarts[i]);
+                //threads[i].Start();
+                //listBox1.Invoke(temp, threads[i].ManagedThreadId);
+                ThreadPool.QueueUserWorkItem(LetsDance1,null);
+            }            
+        }
+        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (groupBox1.Controls.Count > 0) 
+            { 
+                groupBox1.Controls.Clear(); 
+            }
+            PlacementBars(comboBox1.Text);
+        }
+        public void PlacementBars(string number)
+        {
+            if(verticals.Count>0)
+            {
+                verticals.Clear();
+            }
+            int num = Int32.Parse(number);
+            Point positionBar = new Point(0, 20)
+            {
+                X = (groupBox1.Width / 2 - (sizeBar.Width / 2)) - sizeBar.Width * comboBox1.SelectedIndex
+            };
+            for (int i=0; i<num; i++)
+            {
+                verticals.Add(new VerticalProgressBar());
+                verticals[i].Size = sizeBar;
+                verticals[i].Location = positionBar;
+                verticals[i].Step = 1;
+                groupBox1.Controls.Add(verticals[i]);
+                positionBar.X += 2 * sizeBar.Width;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < countThread; i++)
+            {
+                threads[i].Abort();
+                
+            }
+            //timer.Dispose();
+            button2.Enabled = false;
+            comboBox1.Enabled = true;
+            button1.Enabled = true;
+            verticals.Clear();
+            countThread = 0;
+            comboBox1_SelectedIndexChanged(sender, e);
+        }
+    }
+    public class VerticalProgressBar : ProgressBar
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style |= 0x04;
+                return cp;
+            }
+        }       
+    }
+}
